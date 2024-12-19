11f68b74ee6ba20b42f03e07ce8837885a25867f
Nguyễn Mạnh Dũng <trexhundle@gmail.com>
2024-12-19 11:43:11 +0700

    first deploy

---
+++ src/components/home/navigation/footer.tsx
@@ -0,0 +1,143 @@
+import Icons from "@/components/global/icons"
+import { Heart } from 'lucide-react'
+import Link from 'next/link'
+
+const Footer = () => {
+    return (
+        <footer className="flex flex-col relative items-center justify-center border-t border-border pt-16 pb-8 px-6 lg:px-8 w-full max-w-6xl mx-auto lg:pt-32">
+
+            <div className="hidden lg:block absolute -top-1/3 -right-1/4 bg-primary w-72 h-72 rounded-full -z-10 blur-[14rem]"></div>
+            <div className="hidden lg:block absolute bottom-0 -left-1/4 bg-primary w-72 h-72 rounded-full -z-10 blur-[14rem]"></div>
+
+            <div className="grid gap-8 xl:grid-cols-3 xl:gap-8 w-full">
+
+                <div className="flex flex-col items-start justify-start md:max-w-[200px]">
+                    <div className="flex items-start">
+                        <Icons.logo className="w-7 h-7" />
+                    </div>
+                    <p className="text-muted-foreground mt-4 text-sm text-start">
+                        Build beautiful, functional websites, without writing code
+                    </p>
+                    <span className="mt-4 text-neutral-200 text-sm flex items-center">
+                        Sản phẩm mẫu Vietfuture
+                    </span>
+                </div>
+
+                <div className="grid-cols-2 gap-8 grid mt-16 xl:col-span-2 xl:mt-0">
+                    <div className="md:grid md:grid-cols-2 md:gap-8">
+                        <div className="">
+                            <h3 className="text-base font-medium text-white">
+                                Sản phẩm
+                            </h3>
+                            <ul className="mt-4 text-sm text-muted-foreground">
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Danh sách nổi trội
+                                    </Link>
+                                </li>
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Giá trung bình
+                                    </Link>
+                                </li>
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Các sản phẩm mẫu
+                                    </Link>
+                                </li>
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Kết nối người bán
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="mt-10 md:mt-0 flex flex-col">
+                            <h3 className="text-base font-medium text-white">
+                                Mạng Xã Hội
+                            </h3>
+                            <ul className="mt-4 text-sm text-muted-foreground">
+                                <li className="">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Facebook
+                                    </Link>
+                                </li>
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Instagram
+                                    </Link>
+                                </li>
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Twitter
+                                    </Link>
+                                </li>
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        LinkedIn
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="md:grid md:grid-cols-2 md:gap-8">
+                        <div className="">
+                            <h3 className="text-base font-medium text-white">
+                                New Feed
+                            </h3>
+                            <ul className="mt-4 text-sm text-muted-foreground">
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Blog
+                                    </Link>
+                                </li>
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Hướng Dẫn
+                                    </Link>
+                                </li>
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Đường dây hỗ trợ
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="mt-10 md:mt-0 flex flex-col">
+                            <h3 className="text-base font-medium text-white">
+                                Thông Tin Trang
+                            </h3>
+                            <ul className="mt-4 text-sm text-muted-foreground">
+                                <li className="">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Về công ty
+                                    </Link>
+                                </li>
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Chính sách
+                                    </Link>
+                                </li>
+                                <li className="mt-2">
+                                    <Link href="" className="hover:text-foreground transition-all duration-300">
+                                        Điều khoản
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="mt-8 border-t border-border/40 pt-4 md:pt-8 md:flex md:items-center md:justify-between w-full">
+                <p className="text-sm text-muted-foreground mt-8 md:mt-0">
+                    &copy; {new Date().getFullYear()} STACK MARKET INC. bản quyền đã đăng ký với PDU, Vietfuture, Hackathon
+                </p>
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/constants/index.ts
@@ -0,0 +1,145 @@
+import { Icons } from "@/components";
+
+export const perks = [
+    {
+        icon: Icons.auth,
+        title: "Đăng Ký",
+        info: "Hướng lên đầu và tạo tài khoản mới theo hướng nhà buôn bán.",
+    },
+    {
+        icon: Icons.customize,
+        title: "Chỉnh Sửa Cửa Hàng",
+        info: "Chọn những hình ảnh bìa và chỉnh sửa nội dung trang bán hàng cá nhân",
+    },
+    {
+        icon: Icons.launch,
+        title: "Đăng Sản Phẩm",
+        info: "Thực hiện Mint sản phẩm và thêm các thông tin metadata để bán sản phẩm",
+    },
+];
+
+export const features = [
+    {
+        icon: Icons.bolt,
+        title: "Dễ Dàng Tiện Lợi",
+        info: "Trang web chạy trên hệ thống Blockchain theo mẫu chuẩn web3",
+    },
+    {
+        icon: Icons.palette,
+        title: "Rõ Ràng",
+        info: "Các tính năng sử dụng rõ ràng và tiện lợi, chia đúng vai trò và chỉ mục",
+    },
+    {
+        icon: Icons.seo,
+        title: "Bảo Mật Cao",
+        info: "Hệ thống Blockchain đảm bảo các thông tin, sản phẩm, quỹ và tiền tệ của bạn được an toàn.",
+    },
+    {
+        icon: Icons.monitor,
+        title: "Đa Nhiệm",
+        info: "Bạn cũng có thể bán và theo dõi các sản phẩm trên chính điện thoại cá nhân.",
+    },
+    {
+        icon: Icons.shop,
+        title: "Thương Mại Hóa",
+        info: "Các công cụ tìm kiếm và đề xuất sản phẩm sẽ có sẵn để sản phẩm của bạn bán chạy hơn.",
+    },
+    {
+        icon: Icons.server,
+        title: "Lưu Trữ",
+        info: "Tất cả các dữ liệu sẽ được lưu ở Cơ Sở Dữ Liệu Phân Tán mà chỉ bạn được phép truy cập",
+    },
+];
+
+export const pricingCards = [
+    {
+        title: "Stack New",
+        description: "Dành cho người mới bắt đầu",
+        price: "Miễn Phí(trọn đời)",
+        duration: "",
+        highlight: "Key features",
+        buttonText: "Bắt đầu khi đăng ký",
+        features: ["Giới hạn 200 sản phẩm", "Có thể tạo 1 cửa hàng", "Gồm các tính năng cơ bản"],
+        priceId: "",
+    },
+    {
+        title: "Professional",
+        description: "Nâng cấp tốt hơn",
+        price: "99.000 VND/mth",
+        duration: "month",
+        highlight: "Key features",
+        buttonText: "Mua Ngay",
+        features: ["Không giới hạn sản phẩm", "Có thể tạo 3 cửa hàng", "Các tính năng nâng cấp", "Hỗ trợ khuyến mãi 5%"],
+        priceId: "price_1OYxkqFj9oKEERu1KfJGWxgN",
+    },
+    {
+        title: "Doanh Nghiệp",
+        description: "Dành cho các tổ chức lớn",
+        price: "399.000 VND/mth",
+        duration: "month",
+        highlight: "Everything in Starter, plus",
+        buttonText: "Upgrade to Enterprise",
+        features: ["Không giới hạn sản phẩm", "Có thể tạo 6 cửa hàng", "Nâng cấp bản Pro", "Event khuyến mãi riêng ", "Chức năng kêu gọi dự án", "Có domain name riêng"],
+        priceId: "price_1OYxkqFj9oKEERu1NbKUxXxN",
+    },
+];
+
+export const bentoCards = [
+    {
+        title: 'Start with Inspiration',
+        info: 'Browse our vast library of pre-designed templates or upload your own images.',
+        imgSrc: '/assets/bento-1.svg', // Lightbulb or Inspiration icon
+        alt: 'Inspiration for website creation'
+    },
+    {
+        title: 'AI Assists Your Vision',
+        info: 'Our intelligent AI tailors suggestions and functionalities based on your goals.',
+        imgSrc: '/assets/bento1.svg', // AI Assistant icon
+        alt: 'AI website building assistant'
+    },
+    {
+        title: 'Drag & Drop Customization',
+        info: 'Effortlessly personalize your website with our intuitive drag-and-drop editor.',
+        imgSrc: '/assets/bento1.svg', // Drag and Drop icon or hand editing a website
+        alt: 'Website customization with drag and drop'
+    },
+    {
+        title: 'Launch & Shine Online',
+        info: 'Publish your website with a single click and take your brand to the world.',
+        imgSrc: '/assets/bento1.svg', // Rocket launching or website going live
+        alt: 'Website launch and publication'
+    },
+];
+
+export const reviews = [
+    {
+        name: "Jack",
+        username: "@jack",
+        body: "I've never seen anything like this before. It's amazing. I love it.",
+    },
+    {
+        name: "Jill",
+        username: "@jill",
+        body: "I don't know what to say. I'm speechless. This is amazing.",
+    },
+    {
+        name: "John",
+        username: "@john",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+    },
+    {
+        name: "Jane",
+        username: "@jane",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+    },
+    {
+        name: "Jenny",
+        username: "@jenny",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+    },
+    {
+        name: "James",
+        username: "@james",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+    },
+];
